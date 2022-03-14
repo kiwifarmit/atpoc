79716877ae8442454aaa8e2afa24044f67529623
Carlo Perassi <carlo.perassi@gmail.com>
2022-03-14 12:01:53 +0100

    receive, initial

--- contracts/Mlist.sol
+++ contracts/Mlist.sol
@@ -7,6 +7,34 @@ contract Mlist {
     address owner;
     uint hashInternal;
 
+    /*
+    // https://solidity-by-example.org/sending-ether/
+    Which function is called, fallback() or receive()?
+
+           send Ether
+               |
+         msg.data is empty?
+              / \
+            yes  no
+            /     \
+receive() exists?  fallback()
+         /   \
+        yes   no
+        /      \
+    receive()   fallback()
+    */
+
+    // Function to receive Ether. msg.data must be empty
+    receive() external payable {}
+
+    // Fallback function is called when msg.data is not empty
+    fallback() external payable {
+        emit Paid(msg.sender, msg.value);
+    }
+
+    /// @notice Logs the address of the sender and amounts paid to the contract
+    event Paid(address indexed _from, uint _value);
+
     constructor(string memory _hashString) {
         owner = msg.sender;
         hashInternal = uint(keccak256(abi.encodePacked(_hashString)));
@@ -16,10 +44,18 @@ contract Mlist {
         return string(abi.encodePacked(a, b));
     }
 
+    function getContractSelfAddress() public view returns (address) {
+        return address(this);
+    }
+
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+
     function gethashInternalAsString() public view returns(string memory) {
         return Strings.toHexString(hashInternal);
     }
-
+    
     function sethashInternal(string memory _hashString) public {
         require(msg.sender == owner, "Not the owner");
 
--- test/Mlist.test.js
+++ test/Mlist.test.js
@@ -8,7 +8,22 @@ contract('Mlist', (accounts) => {
     before(async () => {
         mList = await Mlist.deployed()
     })
+
+    // getContractSelfAddress
+    it('should return the contract address', async () => {
+        const address = await mList.getContractSelfAddress()
+        console.log(address)
+        assert.equal(isNaN(address), false)
+    })
+
+    // getBalance
+    it('should return the current balance', async () => {
+        const balance = await mList.getBalance()
+        console.log(balance)
+        assert.equal(isNaN(balance.toNumber()), false)
+    })
     
+    // gethashInternalAsString
     it('It should return 0xc5d...470', async () => {
         const hashInternalAsString = await mList.gethashInternalAsString()
 
@@ -16,6 +31,7 @@ contract('Mlist', (accounts) => {
         assert.equal(hashInternalAsString, emptyStringHash)
     })
     
+    // gethashInternalAsString
     it('It should set hashInternal to 0xbea...3ae', async() => {
         const combinedString = '0xbea7774072254bb0d224ea4ea5daeabc6b2520620334696fde6408429355e3ae' // void + 'a'
 
@@ -24,6 +40,45 @@ contract('Mlist', (accounts) => {
         assert.equal(hashInternalAsString, combinedString)
     })
 
+    // pay
+    it('It should pay 1 ETH to contract', async() => {
+        const one_eth = web3.utils.toWei('1', "ether");
+        const accounts = await web3.eth.getAccounts()
+        const contract_address = await mList.getContractSelfAddress()
+
+        const balance_wei_before = await web3.eth.getBalance(contract_address);
+        const balance_ether_before = web3.utils.fromWei(balance_wei_before, "ether");
+
+        await web3.eth.sendTransaction({from: accounts[1], to: contract_address, value: one_eth});
+        const balance_wei_after = await web3.eth.getBalance(contract_address);
+        const balance_ether_after = web3.utils.fromWei(balance_wei_after, "ether");
+        console.log('balance_ether_before', balance_ether_before)
+        console.log('balance_ether_after', balance_ether_after)
+        assert.equal(balance_ether_after, Number(balance_ether_before) + 1);
+    })
+
+    // TODO
+    // https://trufflesuite.com/docs/truffle/getting-started/interacting-with-your-contracts#making-a-transaction
+    // truffle(develop)> let accounts = await web3.eth.getAccounts()
+    // truffle(develop)> instance.sendCoin(accounts[1], 10, {from: accounts[0]})
+
+    /*// send
+    it('should send 1 ether to the contract', async () => {
+        mList.send(web3.utils.toWei(1, "ether")).then(result => {
+            console.log(result)
+        })
+
+        // const balanceBefore = await mList.getBalance()
+        // console.log('balanceBefore', balanceBefore)
+        // const amount = 1
+        // const receiver = accounts[1]
+        // await mList.send(receiver, amount, {from: accounts[0]})
+        // const balanceAfter = await mList.getBalance()
+        // const balanceDifference = balanceAfter.toNumber() - balanceBefore.toNumber()
+        // assert.equal(balanceDifference, amount)
+    })*/
+
+    // sethashInternal
     it("It should abort with an error", async () => {
         await truffleAssert.reverts(mList.sethashInternal(aString, {from: accounts[1]}), "Not the owner")
     })
